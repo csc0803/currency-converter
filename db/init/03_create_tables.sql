924a0bdb101a0a2643ffac08ae51e0c9ecd3e6cc
LAPTOP-A0LLULTT\Chun <csccoding0803@gmail.com>
2026-04-09 16:45:48 +0800

    db: add exchange_rates and conversion_records table scripts

---
+++ db/init/03_create_tables.sql
@@ -0,0 +1,25 @@
+USE currency_db;
+
+IF NOT EXISTS (
+    SELECT name FROM sys.tables WHERE name = 'exchange_rates'
+)
+BEGIN
+    CREATE TABLE exchange_rates (
+        id              BIGINT IDENTITY(1,1) NOT NULL,
+        base_currency   CHAR(3)              NOT NULL,
+        target_currency CHAR(3)              NOT NULL,
+        rate            DECIMAL(18, 6)       NOT NULL,
+        updated_at      DATETIME2            NOT NULL,
+
+        CONSTRAINT PK_exchange_rates PRIMARY KEY (id),
+
+        -- 同一個幣別對只存一筆，更新時用 MERGE
+        CONSTRAINT UQ_exchange_rates_pair UNIQUE (base_currency, target_currency)
+    );
+
+    PRINT 'exchange_rates 建立成功';
+END
+ELSE
+BEGIN
+    PRINT 'exchange_rates 已存在，略過';
+END
